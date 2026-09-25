28f408851627ab47e2b017bd4228a5f74a611873
namhyuk <lnh929@naver.com>
2026-09-26 03:16:39 +0900

    [Init] : KIRI iOS 프로젝트 초기 설정

---
+++ KIRI/KIRI/KIRIApp.swift
@@ -0,0 +1,17 @@
+//
+//  KIRIApp.swift
+//  KIRI
+//
+//  Created by namhyuk on 9/26/26.
+//
+
+import SwiftUI
+
+@main
+struct KIRIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
